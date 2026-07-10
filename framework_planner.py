75b1101a6738bbd7a2da8b49af6f89351bb8fca4
csequenc <skip2025nws@gmail.com>
2026-07-10 22:29:36 +0530

    Implement framework planner with structured output

--- framework_planner.py
+++ framework_planner.py
@@ -1,7 +1,10 @@
 import os
+from urllib import response
 
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from schemas import PlannerDecision
 
 load_dotenv()  # Load environment variables from .env file
 
@@ -10,7 +13,36 @@ llm = ChatGroq(
     temperature=0.1
 )
 
-response = llm.invoke("What is AI in 10 words?")
+structured_llm = llm.with_structured_output(PlannerDecision)
 
-print(type(response))
-print(response.response_metadata)
+planner_prompt = PromptTemplate.from_template(
+    """
+You are an AI assistant that helps choose tools from the following list based on the user's request.
+
+Available tools:
+
+{tools}
+
+User Query:
+{query}
+
+Return ONLY valid JSON in this format:
+
+{{
+    "tool": "...",
+    "input": "..."
+}}
+"""
+)
+
+formatted_prompt = planner_prompt.format(
+    tools="calculate, rag_search, none",
+    query="What is climate change?"
+)
+
+decision = structured_llm.invoke(formatted_prompt)
+
+print(type(decision))
+print(decision)
+print(decision.tool)
+print(decision.input)
